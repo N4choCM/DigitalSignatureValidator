9bb3ba5f6b3298b493ea969d1f3a41a265808e99
N4choCM <nachocamposmarti@gmail.com>
2022-09-16 09:07:54 +0200

    Digital Certificates Store developed.

--- src/main/java/pki/core/CertificateStore.java
+++ src/main/java/pki/core/CertificateStore.java
@@ -8,6 +8,9 @@ import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 public class CertificateStore {
     public static CompanyCertificate getCertificateFromFile(String sPath, String sKey){
@@ -15,6 +18,7 @@ public class CertificateStore {
         CompanyCertificate companyCertificate = new CompanyCertificate();
 
         try{
+            //PKCS12 = Almacena contenido del certificado + clave privada.
             KeyStore jks = KeyStore.getInstance("PKCS12");
             InputStream in = new FileInputStream(sPath);
             jks.load(in, sKey.toCharArray());
@@ -35,5 +39,34 @@ public class CertificateStore {
         }
 
         return companyCertificate;
+    }
+
+    public static List<CompanyCertificate> listCertificatesFromStore(){
+        List<CompanyCertificate> companyCertificates = new ArrayList<>();
+
+        try{
+            KeyStore jks = KeyStore.getInstance("Windows-MY", "SunMSCAPI");
+            jks.load(null, null);
+
+            Enumeration<String> enumeration = jks.aliases();
+            while(enumeration.hasMoreElements()){
+                CompanyCertificate companyCertificate = new CompanyCertificate();
+                String sKeyAlias = (String) enumeration.nextElement();
+
+                PrivateKey privateKey = (PrivateKey) jks.getKey(sKeyAlias, null);
+                Certificate[] certificateChain = jks.getCertificateChain(sKeyAlias);
+                X509Certificate publicCertificate = (X509Certificate) certificateChain[0];
+
+                companyCertificate.setsAlias(publicCertificate.getSubjectDN().getName());
+                companyCertificate.setPublicCertificate(publicCertificate);
+                companyCertificate.setPrivateKey(privateKey);
+                companyCertificate.setCertificateChain(certificateChain);
+
+                companyCertificates.add(companyCertificate);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return companyCertificates;
     }
 }
--- src/test/java/test/ServiceTest.java
+++ src/test/java/test/ServiceTest.java
@@ -4,10 +4,12 @@ import pki.bean.CompanyCertificate;
 import pki.core.CertificateStore;
 import pki.util.Constant;
 
+import java.util.List;
+
 public class ServiceTest {
     public static void main(String[] args) {
         try{
-            CompanyCertificate companyCertificate = CertificateStore.getCertificateFromFile(
+            /* CompanyCertificate companyCertificate = CertificateStore.getCertificateFromFile(
                     Constant.CERTIFICATE,
                     Constant.PASSWORD);
 
@@ -15,7 +17,15 @@ public class ServiceTest {
             System.out.println("---------------------------------------------");
             System.out.println(companyCertificate.getPrivateKey().getAlgorithm());
             System.out.println("---------------------------------------------");
-            System.out.println(companyCertificate.getPublicCertificate().toString());
+            System.out.println(companyCertificate.getPublicCertificate().toString()); */
+
+            List<CompanyCertificate> companyCertificates = CertificateStore.listCertificatesFromStore();
+            for(CompanyCertificate companyCertificate : companyCertificates){
+                System.out.println(companyCertificate.getsAlias());
+                System.out.println(companyCertificate.getPublicCertificate().getIssuerDN());
+                System.out.println(companyCertificate.getPublicCertificate().getNotAfter());
+                System.out.println("---------------------------------------------");
+            }
         } catch (Exception e){
             e.printStackTrace();
         }
